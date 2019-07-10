b62441ad48de40a360ff2c74e9a16a48683632c9
jeremiahnow <jeremiahnnadi@icloud.com>
2019-07-10 14:55:54 +0100

    JS fixes

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,24 @@
-var appnav = new Vue({
-    el: '#app-nav',
-    data: {
-        message: "Click this link to enter Jeremiah's world"
+/* function toggleAlert(){
+    var alertSuccess = document.getElementById('alert-success');
+    var displaySetting = alertSuccess.style.display;
+    var sendButton = document.getElementById('sendButton');
+
+    if (displaySetting === 'none') {
+      alertSuccess.style.display = 'block';
+      sendButton.innerHTML = 'Send';
     }
-})
+    else if (displaySetting === 'block'){
+      alertSuccess.style.display = 'none';
+      sendButton.innerHTML = 'Send another email';
+    }
+  }
+  */
+ function toggleAlert(){
+     var alertSuccess = document.getElementById("alert-success");
+
+     if (alertSuccess.style.display === "none") {
+         alertSuccess.style.display = "block";
+     } else {
+         alertSuccess.style.display = "none";
+     }
+ }
