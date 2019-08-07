d442a33179c936bce5fbb7dcfbec8568d1f79ef1
ip1-groupa <52701672+ip1-groupa@users.noreply.github.com>
2019-08-07 15:53:53 +0100

    Add files via upload

---
+++ julian_site/contact.js
@@ -0,0 +1,38 @@
+import 'bootstrap-css-only/css/bootstrap.min.css';
+import 'mdbvue/build/css/mdb.css';
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+});
+
+import { mdbContainer, mdbRow, mdbCol, mdbInput, mdbTextarea, mdbBtn, mdbIcon, mdbModal, mdbModalHeader, mdbModalBody, mdbModalFooter } from 'mdbvue';
+  export default {
+    name: 'FormsPage',
+    components: {
+      mdbContainer,
+      mdbRow,
+      mdbCol,
+      mdbInput,
+      mdbTextarea,
+      mdbBtn,
+      mdbIcon,
+      mdbModal,
+      mdbModalHeader,
+      mdbModalBody,
+      mdbModalFooter
+    },
+    data() {
+      return {
+        showModal: false
+      };
+    }
+  }
